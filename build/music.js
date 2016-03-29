2ab660791170d16c82a466827c7a9a721d9415f9
YaoYin <YaoYin>
2016-03-29 22:20:25 +0800

    l

--- build/music.js
+++ build/music.js
@@ -1,24 +1,26 @@
 document.onreadystatechange = subSomething;//当页面加载状态改变的时候执行这个方法.
 function subSomething()
 {
-if(document.readyState == 'complete'){
+	if(document.readyState == 'complete'){
 
-alert('page loaded complete');
+
+
+	document.addEventListener('touchstart',touch,false);
+	var isPlaying = false;
+	function touch(){
+		if(!isPlaying){
+
+			document.getElementById('Audio').setAttribute('loop','loop');
+			document.getElementById('Audio').play();
+			isPlaying = true;
+		}
+
+	}
 } //当页面加载状态
 
 }
 
-document.addEventListener('touchstart',touch,false);
-var isPlaying = false;
-function touch(){
-	if(!isPlaying){
 
-		document.getElementById('Audio').setAttribute('loop','loop');
-		document.getElementById('Audio').play();
-	}
-
-	alert('ok');
-}
 
 
 
